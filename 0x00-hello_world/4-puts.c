025cec94e8d25bbda6eb087e9b3a2aad227985c9
somtee-tech <somtoanisiuba@gmail.com>
2022-03-11 04:57:03 -0800

    printing usinf puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: always return 0
  */
-	int main(void)
-	{
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-
-	}
+}

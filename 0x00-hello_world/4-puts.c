886e07fb5d94d4efd5c4d78231e93c3f7873e750
somtee-tech <somtoanisiuba@gmail.com>
2022-03-11 04:52:46 -0800

    printing using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-	 
-	 /**
-	  *  * main - Entry point
-	  *   *
-	  *    * Return: Always 0 (Success)
-	  *     */
-	 int main(void)
-	 {
-		 	puts("\"Programming is like building a multilingual puzzle");
-				return (0);
-	 }
- ;
-}
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+	int main(void)
+	{
+
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+
+	}

8a1d19d6cbfc33a93e3223cfe9930b2ab28d288e
somtee-tech <somtoanisiuba@gmail.com>
2022-03-16 17:33:44 -0700

    printing alphabets in lowercase using prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/**
+ * File: 1-alphabet.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/test_run/README.md
@@ -0,0 +1,1 @@
+test_run file folder and update section on README accordingly

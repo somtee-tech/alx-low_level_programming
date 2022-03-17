3537e540525a4082335eea39d7a84d48204c00d0
somtee-tech <somtoanisiuba@gmail.com>
2022-03-16 18:51:58 -0700

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/test_run/9-main.c
@@ -0,0 +1,21 @@
+#include "../main.h"
+
+
+
+/**
+ *
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+
+		times_table();
+
+		return (0);
+
+}

185de1c48c10077aded384ff0ec949dfa7b5addc
somtee-tech <somtoanisiuba@gmail.com>
2022-03-18 06:24:25 -0700

    completed task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers (0-9) except (2,4)
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}

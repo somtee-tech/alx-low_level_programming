212cf994b4e02b0cdf029a415bf2ad7b8e5d6de6
somtee-tech <somtoanisiuba@gmail.com>
2022-03-24 06:42:59 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,21 +13,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int length, j;
+	/* j is a counter for  n bytes of src to be concatenated */
+	/* length = length of destination string */
 
-	/* Store length of destination in the length variable*/
 	length = 0;
 	while (dest[length] != '\0')
 	{
 		length++;
 	}
-
-	/* concatenate src to dest*/
-	for (j = 0; src[j] != '\0'; j++, length++)
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
-
-	/* terminating the dest string*/
 	dest[length] = '\0';
 	return (dest);
 }

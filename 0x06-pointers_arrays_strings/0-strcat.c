d9f79bbd79aca3c1b93636a409fdd8fbe5087e81
somtee-tech <somtoanisiuba@gmail.com>
2022-03-24 06:00:04 -0700

    updated concatenation by adding comments

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,16 +14,20 @@ char *_strcat(char *dest, char *src)
 {
 	int length, j;
 
+	/* Store length of destination in the length variable*/
 	length = 0;
-
 	while (dest[length] != '\0')
 	{
 		length++;
 	}
+
+	/* concatenate src to dest*/
 	for (j = 0; src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
+
+	/* terminating the dest string*/
 	dest[length] = '\0';
 	return (dest);
 }

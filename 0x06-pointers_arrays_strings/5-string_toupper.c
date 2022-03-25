2799a3de48d9febf99b5312737b36c11b37af498
somtee-tech <somtoanisiuba@gmail.com>
2022-03-25 06:19:38 -0700

    String Toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase.* @str: pointer to string.
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase.
+ * @str: pointer to string.
  *
  * Return: pointer to uppercase string.
  */

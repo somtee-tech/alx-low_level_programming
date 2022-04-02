682af8e1920794539af7fa853bc632185a4d963d
somtee-tech <somtoanisiuba@gmail.com>
2022-04-01 21:01:58 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
 /**
-
  * main - multiply two numbers
-
  * @argc: arguement count
-
  * @argv: array of pointers to arguement strings
-
  * Return: result of multiplication or 1
-
- **/
+ */
 
 int main(int argc, char *argv[])
-
 {
 
 	int a, b, result;

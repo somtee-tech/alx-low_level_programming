2ee6efcf6aac9830e68f2c760a428a22d9e3379e
somtee-tech <somtoanisiuba@gmail.com>
2022-04-01 20:54:56 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 /**
-
  * main - a program that prints all arguments it receives.
-
  * @argc: arguement count
-
  * @argv: array of pointers to arguement strings
-
  * Return: 0
-
- **/
+ */
 
 int main(int argc, char *argv[])
-
 {
 
 	while (argc--)

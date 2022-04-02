6eb5d574f430f5253c114b4c427d22afe350b99e
somtee-tech <somtoanisiuba@gmail.com>
2022-04-01 18:32:27 -0700

    create static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include 'main.h"
+
+/**
+ * _puts - prints a string of characters
+ * @str: pointer to a string
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

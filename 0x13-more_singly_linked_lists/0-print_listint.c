7011e641d6f4c28dce0b8f6117e2d3da93ea8ab9
somtee-tech <somtoanisiuba@gmail.com>
2022-04-26 16:38:28 -0700

    add print list file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - prints all the elements of a listint list
+ * @h: the linked list to print
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d ", h->n);
+		printf("\n");
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Creating More singly linked lists in C
